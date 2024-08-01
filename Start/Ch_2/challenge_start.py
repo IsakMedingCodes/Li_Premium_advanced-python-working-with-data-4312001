4725e59cf141d584189b3782afc02a359228803b
IsakMedingCodes <73895335+IsakMedingCodes@users.noreply.github.com>
2024-08-01 09:16:22 +0000

    Ch2_complete

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,20 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
+from collections import Counter
 
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+events = data["features"]
+
+c=Counter([e["properties"]["type"] for e in events])
+
+print(c)
+print(f'earthquake      : {c["earthquake"]}')
+print(f'explosion       : {c["explosion"]}')
+print(f'quarry blast    : {c["quarry blast"]}')
+print(f'ice quake       : {c["ice quake"]}')
+print(f'other event     : {c["other event"]}')
